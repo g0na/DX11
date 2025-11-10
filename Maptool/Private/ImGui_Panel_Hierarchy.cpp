53fbb7c17f61c72258b7688985d6f5cbab278924
gona <dlcndgh70@gmail.com>
2025-11-10 21:54:13 +0900

    feat: 선택한 오브젝트명 나오도록 추가

--- Maptool/Private/ImGui_Panel_Hierarchy.cpp
+++ Maptool/Private/ImGui_Panel_Hierarchy.cpp
@@ -22,12 +22,12 @@ void CImGui_Panel_Hierarchy::Render()
     _uint iCurLevelID = m_pGameInstance->Get_CurLevelID();
     map<const _wstring, CBase*> mapPrototypes = m_pGameInstance->Get_Prototypes(iCurLevelID);
 
-    ImGui::SetNextWindowSize(ImVec2(500, 350), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(700, 350), ImGuiCond_FirstUseEver);
     if (ImGui::Begin(m_strLabel.c_str()))
     {
         // Left
         {
-            ImGui::BeginChild("left pane", ImVec2(300, 0), ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeX);
+            ImGui::BeginChild("left pane", ImVec2(350, 0), ImGuiChildFlags_Borders | ImGuiChildFlags_ResizeX);
             for (auto& Pair : mapPrototypes)
             {
                 if (dynamic_cast<CGameObject*>(Pair.second) != nullptr)
@@ -35,8 +35,9 @@ void CImGui_Panel_Hierarchy::Render()
                     _char szPrototypeName[128] = {};
                     WideCharToMultiByte(CP_ACP, 0, Pair.first.c_str(), -1, szPrototypeName, sizeof(szPrototypeName), nullptr, nullptr);
                     sprintf_s(szPrototypeName, sizeof(szPrototypeName), szPrototypeName);
+
                     if (ImGui::Selectable(szPrototypeName, false, ImGuiSelectableFlags_SelectOnNav))
-                        int a = 10;
+                        strcpy_s(m_szSelectedObj, szPrototypeName);
                 }
             }
             ImGui::EndChild();
@@ -51,14 +52,9 @@ void CImGui_Panel_Hierarchy::Render()
             ImGui::Separator();
             if (ImGui::BeginTabBar("##Tabs", ImGuiTabBarFlags_None))
             {
-                if (ImGui::BeginTabItem("Description"))
+                if (ImGui::BeginTabItem("Selected Object"))
                 {
-                    ImGui::TextWrapped("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ");
-                    ImGui::EndTabItem();
-                }
-                if (ImGui::BeginTabItem("Details"))
-                {
-                    ImGui::Text("ID: 0123456789");
+                    ImGui::TextWrapped(m_szSelectedObj);
                     ImGui::EndTabItem();
                 }
                 ImGui::EndTabBar();
--- Maptool/Public/ImGui_Panel_Hierarchy.h
+++ Maptool/Public/ImGui_Panel_Hierarchy.h
@@ -3,7 +3,7 @@
 
 NS_BEGIN(Maptool)
 
-// 현재 씬의 오브젝트 계층을 트리 형태로 보여주는 창
+// 현재 씬의 원본들을 보여주는 창
 class CImGui_Panel_Hierarchy final : public CImGui_Panel
 {
 private:
@@ -12,6 +12,9 @@ private:
 public:
 	virtual HRESULT Initialize() override;
 	virtual void Render() override;
+
+private:
+	_char	m_szSelectedObj[128] = {};
 
 public:
 	static CImGui_Panel_Hierarchy* Create();
