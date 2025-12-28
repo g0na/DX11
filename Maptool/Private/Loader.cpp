9be193610345477bf7bc8599e4b5acc6ee94651f
g0na <dlcndgh70@gmail.com>
2025-12-28 17:46:57 +0900

    chore: 맵 파츠 전부 추가 완료

--- Maptool/Private/Loader.cpp
+++ Maptool/Private/Loader.cpp
@@ -131,7 +131,7 @@ HRESULT CLoader::Loading_GamePlay()
 
 	/* For.Prototype_Component_Model_Map2 */
 	if (FAILED(m_pGameInstance->Add_Prototype(ENUM_TO_UINT(LEVELID::GAMEPLAY), TEXT("Prototype_Component_Model_Map2"),
-		CModel::Create(m_pDevice, m_pContext, MODEL::NONANIM, "../Bin/Resources/Models/Maps/map_part3.fbx", PreTransformMatrix))))
+		CModel::Create(m_pDevice, m_pContext, MODEL::NONANIM, "../Bin/Resources/Models/Maps/map_part5.fbx", PreTransformMatrix))))
 		return E_FAIL;
 	
 	/* For.Prototype_Component_Calculator */
